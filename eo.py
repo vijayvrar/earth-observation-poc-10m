6b1f8120aa7d234166380adff15e103078a9c938
vijayvrar <83647741+vijayvrar@users.noreply.github.com>
2026-09-23 17:33:06 +0530

    Update eo.py

--- eo.py
+++ eo.py
@@ -54,7 +54,7 @@ def create_rgb_image(
     # 800 x 10 m = approximately 8 km
     # --------------------------------------------------------
 
-    crop_size = 400
+    crop_size = 600
     half = crop_size // 2
 
 
